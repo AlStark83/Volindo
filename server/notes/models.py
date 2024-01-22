87f0db472c89d2f18cd8e774b9529aafa458a1dc
Alberto Guerrero Covarrubias <a.guerrero.c.83@gmail.com>
2024-01-22 00:14:05 -0600

    Title don't repeat, is unique on DB

--- server/notes/models.py
+++ server/notes/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Notes(models.Model):
-  title = models.CharField(max_length=100)
+  title = models.CharField(max_length=100, unique=True)
   description = models.TextField(max_length=1000)
